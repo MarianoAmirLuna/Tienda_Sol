51b441202e59fc031a6786be6fd8943e121e5218
MarianoAmirLuna <mlunaregalado@frba.utn.edu.ar>
2025-09-23 04:26:37 +0000

    Mas tests sobre pedido, lógica movida de controller a service y persistencia del historial de estados

--- tests/pedido.test.js
+++ tests/pedido.test.js
@@ -5,6 +5,7 @@ import {DireccionEntrega} from "../models/entities/pedido/direccionEntrega.js";
 import {Producto} from "../models/entities/producto/producto.js";
 import {Categoria} from "../models/entities/producto/categoria.js";
 import {Pedido} from "../models/entities/pedido/pedido.js";
+import {EstadoPedido} from "../models/entities/pedido/estadoPedido.js";
 
 const direccionDeEntregaGlobal = new DireccionEntrega(
     "Av. Siempre Viva",
@@ -38,6 +39,28 @@ function productoConStock(stockDeseado) {
     );
 }
 
+function productoConID(id) {
+    const p = new Producto(
+        1,
+        "Zapatillas deportivas",
+        "Zapatillas de running, talle 42, color azul",
+        [
+            new Categoria("deporte"),
+            new Categoria("calzado")
+        ],
+        35000,
+        "Chelines",
+        30,
+        [
+            "https://example.com/img/zapatillas1.jpg",
+            "https://example.com/img/zapatillas2.jpg"
+        ],
+        true
+    );
+    p.setId(id);
+    return p
+}
+
 function pedidoConItemPedido(itemDeseado) {
     return new Pedido(
         1,
@@ -46,6 +69,19 @@ function pedidoConItemPedido(itemDeseado) {
         "Chelines",
         direccionDeEntregaGlobal
     );
+}
+
+function pedidoConID(idDeseado) {
+    const p = new Pedido(
+        1,
+        [new ItemPedido(1, 20, 100)],
+        2000,
+        "Chelines",
+        direccionDeEntregaGlobal
+    );
+    p.setId(idDeseado);
+    return p;
+
 }
 
 
@@ -77,7 +113,7 @@ describe("PedidoService", () => {
                 p.setId(id);
                 return p;
             }),
-            actualizar: jest.fn().mockImplementation(async (id, nuevoStock) => productoFinal)
+            actualizar: jest.fn().mockImplementation(async (id, nuevoStock) => {})
         };
 
         const pedidoService = new PedidoService(pedidoRepository, productoRepository);
@@ -131,4 +167,137 @@ describe("PedidoService", () => {
             `El producto de id 2 no existe como producto`
         );
     });
+
+    test("Obtener todos los pedidos", async () => {
+        // SET UP
+        const pedidoFront1 = pedidoConItemPedido(new ItemPedido(1, 30, 100));
+        const pedidoFront2 = pedidoConItemPedido(new ItemPedido(2, 30, 100));
+        const pedidoFront3 = pedidoConItemPedido(new ItemPedido(3, 30, 100));
+
+        const listaDePedidosFinal = [pedidoFront1,
+            pedidoFront2,
+            pedidoFront3];
+
+        const pedidoRepository = {
+            getPedidos: jest.fn().mockImplementation(async () => listaDePedidosFinal)
+        };
+
+        const pedidoService = new PedidoService(pedidoRepository, {});
+
+        // Me aseguro que haya retornado lo esperado
+        const pedidos = await pedidoService.listarPedidos();
+        expect(pedidos).toEqual(listaDePedidosFinal);
+    });
+
+    test("Obtener 3 pedidos solo por ID", async () => {
+        // SET UP
+        const pedidoFront1 = pedidoConID(1);
+        const pedidoFront2 = pedidoConID(2);
+        const pedidoFront3 = pedidoConID(3);
+
+        const pedidoRepository = {
+            findById: jest.fn().mockImplementation(async (id) => {
+                if (id === 1) return pedidoFront1;
+                if (id === 2) return pedidoFront2;
+                if (id === 3) return pedidoFront3;
+                return null;
+            })
+        };
+
+        const pedidoService = new PedidoService(pedidoRepository, {});
+
+        const pedidos1 = await pedidoService.obtenerPedido(1);
+        expect(pedidoRepository.findById).toHaveBeenCalledWith(1);
+        const pedidos2 = await pedidoService.obtenerPedido(2);
+        expect(pedidoRepository.findById).toHaveBeenCalledWith(2);
+        const pedidos3 = await pedidoService.obtenerPedido(3);
+        expect(pedidoRepository.findById).toHaveBeenCalledWith(3);
+
+        // Me aseguro que haya retornado lo esperado
+        expect(pedidos1).toEqual(pedidoFront1);
+        expect(pedidos2).toEqual(pedidoFront2);
+        expect(pedidos3).toEqual(pedidoFront3);
+    });
+
+    /* No esta hecha la lógica de eliminar pedido, grande JS que no nos advirtio
+    test("Eliminar 1 pedido por su ID", async () => {
+        // SET UP
+        const pedidoFront1 = pedidoConID(1);
+        const pedidoFront2 = pedidoConID(2);
+        const pedidoFront3 = pedidoConID(3);
+
+        const pedidoRepository = {
+            findById: jest.fn().mockImplementation(async (id) => {
+                if (id === 1) return pedidoFront1;
+                if (id === 2) return pedidoFront2;
+                if (id === 3) return pedidoFront3;
+                return null;
+            })
+        };
+
+        const pedidoService = new PedidoService(pedidoRepository, {});
+
+    });
+    */
+
+    test("Cancelar un Pedido exitosamente", async () => {
+        // SET UP
+        const pedidoFront = pedidoConID(1);
+        const pedidoFinal = pedidoConID(1);
+        pedidoFinal.cambiarEstado(EstadoPedido.CANCELADO)
+
+        const pedidoRepository = {
+            actualizar: jest.fn().mockImplementation(async () => {})
+        };
+
+        const pedidoService = new PedidoService(pedidoRepository, {});
+
+        const pedidoCancelado = await pedidoService.cancelarPedido(pedidoFront);
+        expect(pedidoRepository.actualizar).toHaveBeenCalledWith(pedidoFinal);
+        expect(pedidoCancelado).toEqual(pedidoFinal);
+    });
+
+    test("Cancelar un Pedido fallidamente", async () => {
+        // SET UP
+        const pedidoFront = pedidoConID(1);
+        pedidoFront.cambiarEstado(EstadoPedido.ENVIADO);
+
+        const pedidoService = new PedidoService({}, {});
+
+        const pedidoCancelado = await pedidoService.cancelarPedido(pedidoFront);
+        expect(pedidoCancelado).toEqual(null);
+    });
+
+    test("Enviar un Pedido exitosamente", async () => {
+        // SET UP
+        const pedidoFront = pedidoConID(1);
+        const pedidoFinal = pedidoConID(1);
+        pedidoFinal.cambiarEstado(EstadoPedido.ENVIADO)
+
+        const pedidoRepository = {
+            actualizar: jest.fn().mockImplementation(async () => {})
+        };
+        const productoRepository = {
+            findById: jest.fn().mockImplementation(async (id) => productoConID(1))
+        };
+
+        const pedidoService = new PedidoService(pedidoRepository, productoRepository);
+
+        const pedidoEnviado = await pedidoService.enviarPedido(pedidoFront);
+        expect(pedidoRepository.actualizar).toHaveBeenCalledWith(pedidoFinal);
+        expect(productoRepository.findById).toHaveBeenCalledWith(1);
+        expect(pedidoEnviado).toEqual(pedidoFinal);
+    });
+
+/*
+    test("Enviar un Pedido fallidamente", async () => {
+        // SET UP
+        const pedidoFront = pedidoConID(1);
+        pedidoFront.cambiarEstado(EstadoPedido.ENVIADO);
+
+        const pedidoService = new PedidoService({}, {});
+
+        const pedidoCancelado = await pedidoService.cancelarPedido(pedidoFront);
+        expect(pedidoCancelado).toEqual(null);
+    });*/
 });
