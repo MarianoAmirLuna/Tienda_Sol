c1935ebcc783e536862885c890b4de0762e67229
gianlucca bolocco <gbolocco@frba.utn.edu.ar>
2025-10-23 14:49:02 -0300

    tabla gestion de productos vendedor

--- tiendaSolFrontend/src/pages/SellerPanelPage.jsx
+++ tiendaSolFrontend/src/pages/SellerPanelPage.jsx
@@ -1,6 +1,7 @@
 import SalesTable from "../components/SellerPanel/SalesTable/SalesTable";
 import CreateProduct from "../components/SellerPanel/CreateProductForm/CreateProduct";
 import { useUser } from "../context/UserContext";
+import ProductTable from "../components/SellerPanel/ProductTable/ProductTable";
 
 export default function SellerPanelPage() {
 
@@ -10,7 +11,7 @@ export default function SellerPanelPage() {
 
 
     return (
-        <div className="min-h-screen bg-neutral-50 dark:bg-neutral-900 text-neutral-900 dark:text-white p-8">
+        <div className="bg-neutral-50 dark:bg-neutral-900 text-neutral-900 dark:text-white p-8">
         <h1 className="text-center text-3xl font-bold mb-6">Bienvenido {usuario?.nombre}!</h1>
 
 
@@ -22,6 +23,11 @@ export default function SellerPanelPage() {
         <section>
             <h2 className="text-2xl font-semibold mb-4">Ventas / Pedidos</h2>
             <SalesTable sellerId={usuario?._id} />
+        </section>
+
+        <section>
+            <h2 className="text-2xl font-semibold mb-4">Mis Productos</h2>
+            <ProductTable sellerId={usuario?._id} />
         </section>
         </div>
     );
